1fd76a5cda000c7317c907dbe9595f675f14c8aa
fangjun <fangjund365@163.com>
2017-03-15 23:04:39 +0800

    优化代码 note1

--- notes/1.遍历出两个时间之间的日期.js
+++ notes/1.遍历出两个时间之间的日期.js
@@ -1,34 +1,40 @@
         // 遍历出两个给定日期之间的所有日期（可以按年、月、日分类）
 		function getMyDate(startDate,endDate,type){
 		  
-		  var startTime = new Date(startDate);
-		  var endTime = new Date(endDate);
+		  var startTime = new Date(startDate),
+		  		endTime = new Date(endDate);
+			if(startTime.getTime()>endTime.getTime()){
+				console.log("开始时间不能大于结束时间！");
+				return false;
+			}		
+			
 		  var dates=[];		  
 		  
 		  switch (type){
 		    case "year":
-		      while((endTime.getTime()-startTime.getTime())>=0){
+					startTime=new Date(startTime.getFullYear().toString());
+					endTime=new Date(endTime.getFullYear().toString());
+		      while(endTime.getTime()-startTime.getTime()>=0){						
 		        var year = startTime.getFullYear();          
 		        dates.push(year);		        
-		        startTime.setYear(Number(year)+1);		        
+		        startTime.setYear(year+1);		        
 		      }        
 		      break;
 		    case "month":
 		      while((endTime.getTime()-startTime.getTime())>=0){
-		        var year = startTime.getFullYear();
-		        var month = startTime.getMonth().toString().length==1?"0"+startTime.getMonth().toString():startTime.getMonth();
-		        	month = Number(month)+1;
+		        var year = startTime.getFullYear(),
+						    month = startTime.getMonth()+1;		        
+		        if(month<10) month="0"+month;
 		        dates.push(year+"-"+month);
 		        startTime.setMonth(month);
 		      }
 		      break;
 		    case "day":
 		      while((endTime.getTime()-startTime.getTime())>=0){
-		        var year = startTime.getFullYear();
-		        var month = startTime.getMonth().toString().length==1?"0"+startTime.getMonth().toString():startTime.getMonth();
-		        month = Number(month)+1;
-		        if(month<10) month='0'+month;
-		        var day = startTime.getDate().toString().length==1?"0"+startTime.getDate():startTime.getDate();
+		        var year = startTime.getFullYear(),
+		            month = startTime.getMonth()+1,
+								day = startTime.getDate().toString().length==1?"0"+startTime.getDate():startTime.getDate();		        
+		        if(month<10) month='0'+month;		        
 		        dates.push(year+"-"+month+"-"+day);
 		        startTime.setDate(startTime.getDate()+1);
 		      }
@@ -37,7 +43,7 @@
 		  
 		  return dates;		  
 		};
-		var date1=getMyDate("2016-12-01","2017-01-01","year");console.log("输出2016-12-01~2017-01-01之间年份：",date1);
+		var date1=getMyDate("2017-12-01","2017-02-01","year");console.log("输出2016-12-01~2017-02-01之间年份：",date1);
         var date2=getMyDate("2016-12-01","2017-01-01","month");console.log("输出2016-12-01~2017-01-01之间月份：",date2);
         var date3=getMyDate("2016-12-01","2017-01-01","day");console.log("输出2016-12-01~2017-01-01之间日期：",date3);
 		
